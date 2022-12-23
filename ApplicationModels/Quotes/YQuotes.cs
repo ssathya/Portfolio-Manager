59795669510f2b53d81acf87bb0a9e503ad21f04
Sridhar Sathya <sridhar.sathya@live.com>
2022-12-22 21:12:43 -0500

    Fixes PV-40

---
+++ ApplicationModels/Quotes/YQuotes.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationModels.Quotes;
+
+public class YQuotes : Entity
+{
+    public string Ticker { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public decimal Open { get; set; }
+    public decimal Close { get; set; }
+    public decimal High { get; set; }
+    public decimal Low { get; set; }
+    public long Volume { get; set; }
+}

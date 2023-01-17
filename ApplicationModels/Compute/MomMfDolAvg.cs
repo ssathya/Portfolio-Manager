e95bf7093a350cfc0cbe4adb5865b197dbf70852
Sridhar <sridhar_0314@live.com>
2023-01-17 15:31:57 -0500

    Fixes PV-79

---
+++ ApplicationModels/Compute/MomMfDolAvg.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationModels.Compute;
+
+/// <summary>
+/// Momentum, dollar volume average, money flow
+/// </summary>
+/// <seealso cref="ApplicationModels.Entity" />
+public class MomMfDolAvg : Entity
+{
+    public string Ticker { get; set; } = string.Empty;
+    public decimal Momentum { get; set; }
+    public decimal DollarVolume { get; set; }
+    public decimal MoneyFlow { get; set; }
+}
--- Plugins/PsqlAccess/Migrations/20230117192926_MomMfDolAvg_Added.Designer.cs
+++ Plugins/PsqlAccess/Migrations/20230117192926_MomMfDolAvg_Added.Designer.cs
@@ -6,6 +6,7 @@ using ApplicationModels.FinancialStatement.AlphaVantage;
 using ApplicationModels.Quotes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PsqlAccess;
@@ -15,9 +16,11 @@ using PsqlAccess;
 namespace PsqlAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230117192926_MomMfDolAvg_Added")]
+    partial class MomMfDolAvgAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

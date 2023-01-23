f4dc6024d6de41ba1593682cc278dfff2a74229a
Sridhar <sridhar_0314@live.com>
2023-01-23 16:22:33 -0500

    API for Cash flow and incomes statements created

---
+++ FrontEnd/Presentation/Pages/Industry/FinStatement.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using ExtData = ApplicationModels.FinancialStatement.AlphaVantage;
+
+namespace Presentation.Pages.Industry;
+
+public partial class FinStatement
+{
+    [Inject]
+    public HttpClient? Client { get; set; }
+
+    [Parameter]
+    public string SelectedTicker { get; set; } = string.Empty;
+
+    private ExtData.BalanceSheet CompanyBS = new();
+    protected ExtData.BSReport BalanceSheetReport { get; set; } = new();
+
+    protected override Task OnParametersSetAsync()
+    {
+        return base.OnParametersSetAsync();
+    }
+}
---
+++ FrontEnd/PresentationHelper/Controllers/CashFlowController.cs
@@ -0,0 +1,46 @@
+﻿using ApplicationModels.FinancialStatement.AlphaVantage;
+using Microsoft.AspNetCore.Mvc;
+using PsqlAccess;
+
+namespace PresentationHelper.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CashFlowController : ControllerBase
+{
+    private readonly ILogger<CashFlowController> logger;
+    private readonly IRepository<CashFlow> repository;
+
+    public CashFlowController(ILogger<CashFlowController> logger
+        , IRepository<CashFlow> repository)
+    {
+        this.logger = logger;
+        this.repository = repository;
+    }
+
+    [HttpGet("{ticker}")]
+    public async Task<IActionResult> Get(string ticker)
+    {
+        if (string.IsNullOrEmpty(ticker))
+        {
+            return BadRequest();
+        }
+        CashFlow? cashFlow;
+        try
+        {
+            cashFlow = (await repository.FindAll(r => r.Ticker == ticker.ToUpper().Trim())).FirstOrDefault();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Exception while retrieving data for ticker {ticker}");
+            logger.LogError(ex.Message);
+            return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        }
+        if (cashFlow == null)
+        {
+            logger.LogInformation($"Cash flow statement for security {ticker} not in database");
+            return NotFound();
+        }
+        return Ok(cashFlow);
+    }
+}

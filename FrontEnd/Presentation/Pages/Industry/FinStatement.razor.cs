e10bb956350c21f8d7c438d264b96bb10344159b
Sridhar <sridhar_0314@live.com>
2023-01-24 15:33:32 -0500

    Fixes PV-87

--- FrontEnd/Presentation/Pages/Industry/FinStatement.razor.cs
+++ FrontEnd/Presentation/Pages/Industry/FinStatement.razor.cs
@@ -8,14 +8,56 @@ public partial class FinStatement
     [Inject]
     public HttpClient? Client { get; set; }
 
+    [Inject]
+    protected ILogger<FinStatement>? logger { get; set; }
+
     [Parameter]
     public string SelectedTicker { get; set; } = string.Empty;
 
-    private ExtData.BalanceSheet CompanyBS = new();
     protected ExtData.BSReport BalanceSheetReport { get; set; } = new();
+    protected ExtData.CashFlowReport CashFlowReport { get; set; } = new();
+    protected ExtData.IncomeReport IncomeReport { get; set; } = new();
+    protected string selectedTab = "balanceSheet";
 
-    protected override Task OnParametersSetAsync()
+    protected override async Task OnParametersSetAsync()
     {
-        return base.OnParametersSetAsync();
+        selectedTab = "balanceSheet";
+        if (Client == null || logger == null)
+        {
+            return;
+        }
+        ExtData.BalanceSheet? companyBS;
+        ExtData.CashFlow? companyCF;
+        ExtData.IncomeStatement? companyIS;
+        try
+        {
+            companyBS = await Client.GetFromJsonAsync<ExtData.BalanceSheet>($"api/BalanceSheet/{SelectedTicker}");
+            companyCF = await Client.GetFromJsonAsync<ExtData.CashFlow>($"api/CashFlow/{SelectedTicker}");
+            companyIS = await Client.GetFromJsonAsync<ExtData.IncomeStatement>($"api/IncomeStatement/{SelectedTicker}");
+            BalanceSheetReport = new();
+            CashFlowReport = new();
+            IncomeReport = new();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Error getting financial information for ticker {SelectedTicker}");
+            logger.LogError($"{ex.Message}", ex);
+            return;
+        }
+        if (companyBS == null || companyCF == null || companyIS == null)
+        { return; }
+        BalanceSheetReport = companyBS.AnnualReports
+            .OrderByDescending(x => x.FiscalDateEnding)
+            .First();
+        CashFlowReport = companyCF.AnnualReports
+            .OrderByDescending(x => x.FiscalDateEnding).First();
+        IncomeReport = companyIS.AnnualReports
+            .OrderByDescending(x => x.FiscalDateEnding).First();
+    }
+
+    protected Task OnSelectedTabChanged(string name)
+    {
+        selectedTab = name;
+        return Task.CompletedTask;
     }
 }

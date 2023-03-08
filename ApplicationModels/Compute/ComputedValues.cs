93d76a08ca377c3e646ed722007b089c1137c5b3
Sridhar <sridhar_0314@live.com>
2023-03-08 08:23:41 -0500

    Fixes PV-109

--- ApplicationModels/Compute/ComputedValues.cs
+++ ApplicationModels/Compute/ComputedValues.cs
@@ -1,8 +1,15 @@
-﻿namespace ApplicationModels.Compute;
+﻿using Skender.Stock.Indicators;
+
+namespace ApplicationModels.Compute;
 
 public class ComputedValues
 {
     public DateTime ReportingDate { get; set; }
     public decimal MomentumValue { get; set; }
     public decimal MoneyFlow { get; set; }
+
+    //public RocResult? RocResult { get; set; }
+    public decimal Roc { get; set; }
+
+    public decimal Sma { get; set; }
 }
--- ApplicationModels/Quotes/CompressedQuote.cs
+++ ApplicationModels/Quotes/CompressedQuote.cs
@@ -1,4 +1,6 @@
-﻿namespace ApplicationModels.Quotes;
+﻿using Skender.Stock.Indicators;
+
+namespace ApplicationModels.Quotes;
 
 public class CompressedQuote
 {
@@ -8,4 +10,17 @@ public class CompressedQuote
     public decimal High { get; set; }
     public decimal Low { get; set; }
     public long Volume { get; set; }
+
+    public static implicit operator Quote(CompressedQuote cq)
+    {
+        return new Quote
+        {
+            Date = cq.Date,
+            Open = cq.Open,
+            High = cq.High,
+            Low = cq.Low,
+            Volume = cq.Volume,
+            Close = cq.ClosingPrice
+        };
+    }
 }

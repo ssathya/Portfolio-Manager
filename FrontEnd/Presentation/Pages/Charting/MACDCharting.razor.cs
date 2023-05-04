e70d663b9dc5bfaa345e4a51e107478f341b0b1a
Sridhar <sridhar_0314@live.com>
2023-05-04 14:28:11 -0400

    Locally commint

--- FrontEnd/Presentation/Pages/Charting/MACDCharting.razor.cs
+++ FrontEnd/Presentation/Pages/Charting/MACDCharting.razor.cs
@@ -6,14 +6,15 @@ namespace Presentation.Pages.Charting;
 
 public partial class MACDCharting
 {
-    private const int daysToSkip = 10;
-    private LineChartOptions lineChartOptions = new() { Scales = new ChartScales { X = new ChartAxis { Type = "linear" } } };
-    private readonly List<string> backgroundColors = new() { ChartColor.FromRgba(103, 145, 194, 0.8f), ChartColor.FromRgba(0, 112, 0, 0.8f), ChartColor.FromRgba(112, 0, 0, 0.8f), ChartColor.FromRgba(192, 192, 0, 0.8f), ChartColor.FromRgba(153, 102, 255, 0.2f), ChartColor.FromRgba(255, 159, 64, 0.2f) };
-    private readonly List<string> borderColors = new() { ChartColor.FromRgba(255, 99, 132, 1f), ChartColor.FromRgba(54, 162, 235, 1f), ChartColor.FromRgba(255, 206, 86, 1f), ChartColor.FromRgba(75, 192, 192, 1f), ChartColor.FromRgba(153, 102, 255, 1f), ChartColor.FromRgba(255, 159, 64, 1f) };
+    protected string CompanyName = string.Empty;
     protected Chart<double>? MACDGraph;
     protected LineChart<double>? quoteChart;
-    protected string CompanyName = string.Empty;
     protected string Ticker = string.Empty;
+
+    private const int daysToSkip = 10;
+    private readonly List<string> backgroundColors = new() { ChartColor.FromRgba(103, 145, 194, 0.8f), ChartColor.FromRgba(0, 112, 0, 0.8f), ChartColor.FromRgba(112, 0, 0, 0.8f), ChartColor.FromRgba(192, 192, 0, 0.8f), ChartColor.FromRgba(153, 102, 255, 0.2f), ChartColor.FromRgba(255, 159, 64, 0.2f) };
+    private readonly List<string> borderColors = new() { ChartColor.FromRgba(255, 99, 132, 1f), ChartColor.FromRgba(54, 162, 235, 1f), ChartColor.FromRgba(255, 206, 86, 1f), ChartColor.FromRgba(75, 192, 192, 1f), ChartColor.FromRgba(153, 102, 255, 1f), ChartColor.FromRgba(255, 159, 64, 1f) };
+    private LineChartOptions lineChartOptions = new() { Scales = new ChartScales { X = new ChartAxis { Type = "linear" } } };
 
     [Parameter]
     public Dictionary<string, string>? DisplayValues { get; set; }
@@ -42,55 +43,9 @@ public partial class MACDCharting
         }
     }
 
-    //protected override async Task OnParametersSetAsync()
-    //{
-    //    await DrawCharts();
-    //}
-
-    private async Task DrawCharts()
-    {
-        if (MACDResult == null || Quotes == null || DisplayValues == null)
-        {
-            return;
-        }
-        List<(string Date, double value)> mACD = MACDResult.Skip(daysToSkip)
-                                             .Where((x, i) => i % daysToSkip == 0)
-                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Macd ?? 0))
-                                             .ToList();
-        List<(string Date, double value)> signal = MACDResult.Skip(daysToSkip)
-                                             .Where((x, i) => i % daysToSkip == 0)
-                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Signal ?? 0))
-                                             .ToList();
-        List<(string Date, double value)> histogram = MACDResult.Skip(daysToSkip)
-                                             .Where((x, i) => i % daysToSkip == 0)
-                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Histogram ?? 0))
-                                             .ToList();
-        List<(string Date, double value)> quoteValues = Quotes.Skip(daysToSkip)
-                                             .Where((x, i) => i % daysToSkip == 0)
-                                             .Select(x => (x.Date.ToString("MMM-dd"), (double)x.Close))
-                                             .ToList();
-        await BuildBarChart(mACD, signal, histogram);
-        await BuildPriceChart(quoteValues);
-    }
-
-    private async Task BuildPriceChart(List<(string Date, double value)> quoteValues)
-    {
-        quoteChart ??= new();
-        await quoteChart.Clear();
-        LineChartDataset<double> chartDataSet = new LineChartDataset<double>
-        {
-            Label = "Closing Price",
-            Data = quoteValues.Select(x => x.value).ToList(),
-            BackgroundColor = backgroundColors[0],
-            BorderColor = borderColors,
-            Fill = false,
-            PointRadius = 3,
-            CubicInterpolationMode = "monotone"
-        };
-        await quoteChart.AddLabelsDatasetsAndUpdate(quoteValues.Select(x => x.Date).ToList(), chartDataSet);
-    }
-
-    private async Task BuildBarChart(List<(string Date, double value)> mACD, List<(string Date, double value)> signal, List<(string Date, double value)> histogram)
+    private async Task BuildBarChart(List<(string Date, double value)> mACD,
+        List<(string Date, double value)> signal,
+        List<(string Date, double value)> histogram)
     {
         MACDGraph ??= new();
         await MACDGraph.Clear();
@@ -131,5 +86,48 @@ public partial class MACDCharting
             CubicInterpolationMode = "monotone"
         });
         await MACDGraph.AddLabelsDatasetsAndUpdate(histogram.Select(x => x.Date).ToList(), histogramCharting);
+    }
+
+    private async Task BuildPriceChart(List<(string Date, double value)> quoteValues)
+    {
+        quoteChart ??= new();
+        await quoteChart.Clear();
+        LineChartDataset<double> chartDataSet = new LineChartDataset<double>
+        {
+            Label = "Closing Price",
+            Data = quoteValues.Select(x => x.value).ToList(),
+            BackgroundColor = backgroundColors[0],
+            BorderColor = borderColors,
+            Fill = false,
+            PointRadius = 3,
+            CubicInterpolationMode = "monotone"
+        };
+        await quoteChart.AddLabelsDatasetsAndUpdate(quoteValues.Select(x => x.Date).ToList(), chartDataSet);
+    }
+
+    private async Task DrawCharts()
+    {
+        if (MACDResult == null || Quotes == null || DisplayValues == null)
+        {
+            return;
+        }
+        List<(string Date, double value)> mACD = MACDResult.Skip(daysToSkip)
+                                             .Where((x, i) => i % daysToSkip == 0)
+                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Macd ?? 0))
+                                             .ToList();
+        List<(string Date, double value)> signal = MACDResult.Skip(daysToSkip)
+                                             .Where((x, i) => i % daysToSkip == 0)
+                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Signal ?? 0))
+                                             .ToList();
+        List<(string Date, double value)> histogram = MACDResult.Skip(daysToSkip)
+                                             .Where((x, i) => i % daysToSkip == 0)
+                                             .Select(x => (x.Date.ToString("MMM-dd"), x.Histogram ?? 0))
+                                             .ToList();
+        List<(string Date, double value)> quoteValues = Quotes.Skip(daysToSkip)
+                                             .Where((x, i) => i % daysToSkip == 0)
+                                             .Select(x => (x.Date.ToString("MMM-dd"), (double)x.Close))
+                                             .ToList();
+        await BuildBarChart(mACD, signal, histogram);
+        await BuildPriceChart(quoteValues);
     }
 }

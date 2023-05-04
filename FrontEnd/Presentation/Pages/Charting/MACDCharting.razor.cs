4febc047a74ad721e945c0723dbde6b75bc6ef15
Sridhar <sridhar_0314@live.com>
2023-05-04 14:23:26 -0400

    Fixes PV-118

--- FrontEnd/Presentation/Pages/Charting/MACDCharting.razor.cs
+++ FrontEnd/Presentation/Pages/Charting/MACDCharting.razor.cs
@@ -10,7 +10,7 @@ public partial class MACDCharting
     private LineChartOptions lineChartOptions = new() { Scales = new ChartScales { X = new ChartAxis { Type = "linear" } } };
     private readonly List<string> backgroundColors = new() { ChartColor.FromRgba(103, 145, 194, 0.8f), ChartColor.FromRgba(0, 112, 0, 0.8f), ChartColor.FromRgba(112, 0, 0, 0.8f), ChartColor.FromRgba(192, 192, 0, 0.8f), ChartColor.FromRgba(153, 102, 255, 0.2f), ChartColor.FromRgba(255, 159, 64, 0.2f) };
     private readonly List<string> borderColors = new() { ChartColor.FromRgba(255, 99, 132, 1f), ChartColor.FromRgba(54, 162, 235, 1f), ChartColor.FromRgba(255, 206, 86, 1f), ChartColor.FromRgba(75, 192, 192, 1f), ChartColor.FromRgba(153, 102, 255, 1f), ChartColor.FromRgba(255, 159, 64, 1f) };
-    protected Chart<double>? histogramChart1;
+    protected Chart<double>? MACDGraph;
     protected LineChart<double>? quoteChart;
     protected string CompanyName = string.Empty;
     protected string Ticker = string.Empty;
@@ -69,46 +69,12 @@ public partial class MACDCharting
                                              .Where((x, i) => i % daysToSkip == 0)
                                              .Select(x => (x.Date.ToString("MMM-dd"), (double)x.Close))
                                              .ToList();
-        histogramChart1 ??= new();
-        await histogramChart1.Clear();
-        BarChartDataset<double> histogramCharting = new BarChartDataset<double>
-        {
-            Label = "+ve Histogram",
-            Data = histogram.Select(x => x.value > 0 ? x.value : 0).ToList(),
-            BackgroundColor = backgroundColors[1],
-            BorderColor = borderColors,
-            BorderWidth = 1
-        };
-        await histogramChart1.AddDataSet(new ChartDataset<double>
-        {
-            Label = "-ve Histogram",
-            Data = histogram.Select(x => x.value <= 0 ? x.value : 0).ToList(),
-            BackgroundColor = backgroundColors[2],
-            BorderColor = borderColors,
-            BorderWidth = 1
-        });
-        await histogramChart1.AddDataSet(new LineChartDataset<double>
-        {
-            Label = "MACD",
-            Data = mACD.Select(x => x.value).ToList(),
-            BackgroundColor = backgroundColors[0],
-            BorderColor = borderColors,
-            Fill = false,
-            PointRadius = 1,
-            CubicInterpolationMode = "monotone"
-        });
-        await histogramChart1.AddDataSet(new LineChartDataset<double>
-        {
-            Label = "Signal",
-            Data = signal.Select(x => x.value).ToList(),
-            BackgroundColor = backgroundColors[3],
-            BorderColor = borderColors,
-            Fill = false,
-            PointRadius = 1,
-            CubicInterpolationMode = "monotone"
-        });
-        await histogramChart1.AddLabelsDatasetsAndUpdate(histogram.Select(x => x.Date).ToList(), histogramCharting);
+        await BuildBarChart(mACD, signal, histogram);
+        await BuildPriceChart(quoteValues);
+    }
 
+    private async Task BuildPriceChart(List<(string Date, double value)> quoteValues)
+    {
         quoteChart ??= new();
         await quoteChart.Clear();
         LineChartDataset<double> chartDataSet = new LineChartDataset<double>
@@ -122,5 +88,48 @@ public partial class MACDCharting
             CubicInterpolationMode = "monotone"
         };
         await quoteChart.AddLabelsDatasetsAndUpdate(quoteValues.Select(x => x.Date).ToList(), chartDataSet);
+    }
+
+    private async Task BuildBarChart(List<(string Date, double value)> mACD, List<(string Date, double value)> signal, List<(string Date, double value)> histogram)
+    {
+        MACDGraph ??= new();
+        await MACDGraph.Clear();
+        BarChartDataset<double> histogramCharting = new BarChartDataset<double>
+        {
+            Label = "+ve Histogram",
+            Data = histogram.Select(x => x.value > 0 ? x.value : 0).ToList(),
+            BackgroundColor = backgroundColors[1],
+            BorderColor = borderColors,
+            BorderWidth = 1
+        };
+        await MACDGraph.AddDataSet(new ChartDataset<double>
+        {
+            Label = "-ve Histogram",
+            Data = histogram.Select(x => x.value <= 0 ? x.value : 0).ToList(),
+            BackgroundColor = backgroundColors[2],
+            BorderColor = borderColors,
+            BorderWidth = 1
+        });
+        await MACDGraph.AddDataSet(new LineChartDataset<double>
+        {
+            Label = "MACD",
+            Data = mACD.Select(x => x.value).ToList(),
+            BackgroundColor = backgroundColors[0],
+            BorderColor = borderColors,
+            Fill = false,
+            PointRadius = 1,
+            CubicInterpolationMode = "monotone"
+        });
+        await MACDGraph.AddDataSet(new LineChartDataset<double>
+        {
+            Label = "Signal",
+            Data = signal.Select(x => x.value).ToList(),
+            BackgroundColor = backgroundColors[3],
+            BorderColor = borderColors,
+            Fill = false,
+            PointRadius = 1,
+            CubicInterpolationMode = "monotone"
+        });
+        await MACDGraph.AddLabelsDatasetsAndUpdate(histogram.Select(x => x.Date).ToList(), histogramCharting);
     }
 }
